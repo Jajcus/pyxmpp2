868fed76beaf6a0f2cb0cd38cf5375dc05781daa
Jacek Konieczny <jajcus@jajcus.net>
2004-09-10 10:55:22 +0000

    - tools to make me make the code better :)

---
+++ pylint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+topdir=`dirname $0`
+if [ -z $topdir ]; then
+	topdir="."
+fi
+cd $topdir
+topdir=`pwd`
+cd build/lib.*
+
+FLAGS="--disable-msg W0324,W0322"
+
+export PYLINTRC=$topdir/pylintrc
+if [ -n "$1" ] ; then
+	pylint $FLAGS $1
+else
+	pylint $FLAGS pyxmpp
+fi
