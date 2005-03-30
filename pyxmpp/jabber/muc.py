5c8f1da22562c1cc25e955ac93e14b4ca6f441db
Jacek Konieczny <jajcus@jajcus.net>
2005-03-30 19:54:09 +0000

    - typos fixed and 'sumbit' form allowed for configure_room()

--- pyxmpp/jabber/muc.py
+++ pyxmpp/jabber/muc.py
@@ -675,7 +675,7 @@ class MucRoomState:
         if not form:
             raise ValueError, "No form received" # TODO: ProtocolError
         self.configuration_form = form
-        self.handler.configuration_form_received(self, form)
+        self.handler.configuration_form_received(form)
 
     def process_configuration_form_error(self, stanza):
         """
@@ -731,17 +731,22 @@ class MucRoomState:
     def configure_room(self, form):
         """
         Configure the room using the provided data.
+        Do nothing if the provided form is of type 'cancel'.
 
         :Parameters:
-            - `form`: the configuration parameters. Should be a `submit` form made by filling-in 
-              the configuration form retireved using `self.request_configuration_form`.
+            - `form`: the configuration parameters. Should be a 'submit' form made by filling-in 
+              the configuration form retireved using `self.request_configuration_form` or
+              a 'cancel' form.
         :Types:
             - `form`: `Form`
 
-        :return: id of the request stanza.
+        :return: id of the request stanza or `None` if a 'cancel' form was provieded.
         :returntype: `unicode`
         """
-        if form.type!="submit":
+        
+        if form.type == "submit":
+            return None
+        elif form.type != "submit":
             raise ValueError, "A 'submit' form required to configure a room"
         iq = Iq(to_jid = self.room_jid.bare(), stanza_type = "set")
         query = iq.new_query(MUC_OWNER_NS, "query")
