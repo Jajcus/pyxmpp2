b07e78ca8cbcca57f9e21685904a7d700898646a
Jacek Konieczny <jajcus@jajcus.net>
2005-12-25 19:20:05 +0000

    - do not make up stanza id for <iq/> stanzas of type 'result' or 'error' (although <iq/> stanzas without id are not allowed in XMPP we should not answer them with a generated id)

--- pyxmpp/iq.py
+++ pyxmpp/iq.py
@@ -65,7 +65,7 @@ class Iq(Stanza):
         elif not stanza_type:
             raise StanzaError,"type is required for Iq"
         else:
-            if not stanza_id:
+            if not stanza_id and stanza_type in ("get", "set"):
                 stanza_id=gen_id()
 
         if not xmlnode and stanza_type not in ("get","set","result","error"):
@@ -115,9 +115,10 @@ class Iq(Stanza):
 
         if self.get_type() not in ("set","get"):
             raise StanzaError,"Results may only be generated for 'set' or 'get' iq"
-
+        
         iq=Iq(stanza_type="result", from_jid=self.get_to(),
                 to_jid=self.get_from(), stanza_id=self.get_id())
+
         return iq
 
     def new_query(self,ns_uri,name="query"):
