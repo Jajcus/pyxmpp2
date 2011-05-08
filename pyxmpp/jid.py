a79d2d8c786e87cd17da3cda32b6fad5cb6bc554
Jacek Konieczny <jajcus@jajcus.net>
2011-05-08 22:46:09 +0200

    jid.py rewritten
    Notable changes:
    - cosmetics
    - set attributes only in __new__
    - use .encode("idna") instead of idna.ToASCII and use idna.nameprep
      for each domain label separately

--- pyxmpp/jid.py
+++ pyxmpp/jid.py
@@ -1,5 +1,5 @@
 #
-# (C) Copyright 2003-2010 Jacek Konieczny <jajcus@jajcus.net>
+# (C) Copyright 2003-2011 Jacek Konieczny <jajcus@jajcus.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License Version
@@ -22,7 +22,7 @@ Normative reference:
   - `RFC 3920 <http://www.ietf.org/rfc/rfc3920.txt>`__
 """
 
-__docformat__="restructuredtext en"
+__docformat__ = "restructuredtext en"
 
 import re
 import weakref
@@ -33,10 +33,10 @@ from encodings import idna
 from pyxmpp.xmppstringprep import nodeprep,resourceprep
 from pyxmpp.exceptions import JIDError
 
-node_invalid_re=re.compile(ur"[" u'"' ur"&'/:<>@\s\x00-\x19]",re.UNICODE)
-resource_invalid_re=re.compile(ur"[\s\x00-\x19]",re.UNICODE)
+node_invalid_re = re.compile(ur"[" u'"' ur"&'/:<>@\s\x00-\x19]", re.UNICODE)
+resource_invalid_re = re.compile(ur"[\s\x00-\x19]", re.UNICODE)
 
-def are_domains_equal(a,b):
+def are_domains_equal(domain1, domain2):
     """Compare two International Domain Names.
 
     :Parameters:
@@ -44,9 +44,9 @@ def are_domains_equal(a,b):
 
     :return: True `a` and `b` are equal as domain names."""
 
-    a=idna.ToASCII(a)
-    b=idna.ToASCII(b)
-    return a.lower()==b.lower()
+    domain1 = domain1.encode("idna")
+    domain2 = domain2.encode("idna")
+    return domain1.lower() == domain2.lower()
 
 class JID(object):
     """JID.
@@ -58,13 +58,14 @@ class JID(object):
 
     JID objects are immutable. They are also cached for better performance.
     """
-    cache=weakref.WeakValueDictionary()
-    __slots__=["node","domain","resource","__weakref__"]
-    def __new__(cls,node_or_jid=None,domain=None,resource=None,check=True):
+    cache = weakref.WeakValueDictionary()
+    __slots__ = ("node", "domain", "resource", "__weakref__",)
+    def __new__(cls, node_or_jid = None, domain = None, resource = None,
+                                                                check = True):
         """Create a new JID object or take one from the cache.
 
         :Parameters:
-            - `node_or_jid`: node part of the JID, JID object to copy or
+            - `node_or_jid`: node part of the JID, JID object to copy, or
               Unicode representation of the JID.
             - `domain`: domain part of the JID
             - `resource`: resource part of the JID
@@ -72,139 +73,140 @@ class JID(object):
               compliance.
         """
 
-        if isinstance(node_or_jid,JID):
+        if isinstance(node_or_jid, JID):
             return node_or_jid
 
         if domain is None and resource is None:
-            obj=cls.cache.get(node_or_jid)
+            obj = cls.cache.get(unicode(node_or_jid))
             if obj:
                 return obj
-        else:
-            obj=None
-        if obj is None:
-            obj=object.__new__(cls)
+            
+        obj = object.__new__(cls)
 
         if node_or_jid:
             node_or_jid = unicode(node_or_jid)
-        if (node_or_jid and
-                ((u"@" in node_or_jid) or (u"/" in node_or_jid))):
-            obj.__from_unicode(node_or_jid)
-            cls.cache[node_or_jid]=obj
+        if (node_or_jid and not domain and not resource):
+            node, domain, resource = cls.__from_unicode(node_or_jid)
+            cls.cache[node_or_jid] = obj
         else:
             if domain is None and resource is None:
-                if node_or_jid is None:
-                    raise JIDError,"At least domain must be given"
-                domain=node_or_jid
-                node_or_jid=None
+                raise JIDError,"At least domain must be given"
             if check:
-                obj.__set_node(node_or_jid)
-                obj.__set_domain(domain)
-                obj.__set_resource(resource)
+                node = cls.__prepare_node(node_or_jid)
+                domain = cls.__prepare_domain(domain)
+                resource = cls.__prepare_resource(resource)
             else:
-                object.__setattr__(obj,"node",node_or_jid)
-                object.__setattr__(obj,"domain",domain)
-                object.__setattr__(obj,"resource",resource)
+                node = node_or_jid
+        object.__setattr__(obj, "node", node)
+        object.__setattr__(obj, "domain", domain)
+        object.__setattr__(obj, "resource", resource)
         return obj
-
+    
     def __setattr__(self,name,value):
         raise RuntimeError,"JID objects are immutable!"
 
-    def __from_unicode(self,s,check=True):
-        """Initialize JID object from Unicode string.
+    @classmethod
+    def __from_unicode(cls, data, check = True):
+        """Return jid tuple from an Unicode string.
 
         :Parameters:
-            - `s`: the JID string
+            - `data`: the JID string
             - `check`: when `False` then the JID is not checked for
-              specification compliance."""
-        s1=s.split(u"/",1)
-        s2=s1[0].split(u"@",1)
-        if len(s2)==2:
+              specification compliance.
+              
+        :Return: (node, domain, resource) tuple"""
+        parts1 = data.split(u"/", 1)
+        parts2 = parts1[0].split(u"@", 1)
+        if len(parts2) == 2:
+            node = parts2[0]
+            domain = parts2[1]
             if check:
-                self.__set_node(s2[0])
-                self.__set_domain(s2[1])
-            else:
-                object.__setattr__(self,"node",s2[0])
-                object.__setattr__(self,"domain",s2[1])
+                node = cls.__prepare_node(node)
+                domain = cls.__prepare_domain(domain)
         else:
+            node = None
+            domain = parts2[0]
             if check:
-                self.__set_domain(s2[0])
-            else:
-                object.__setattr__(self,"domain",s2[0])
-            object.__setattr__(self,"node",None)
-        if len(s1)==2:
+                domain = cls.__prepare_domain(domain)
+        if len(parts1) == 2:
+            resource = parts1[1]
             if check:
-                self.__set_resource(s1[1])
-            else:
-                object.__setattr__(self,"resource",s1[1])
+                resource = cls.__prepare_resource(parts1[1])
         else:
-            object.__setattr__(self,"resource",None)
-        if not self.domain:
-            raise JIDError,"Domain is required in JID."
+            resource = None
+        if not domain:
+            raise JIDError, "Domain is required in JID."
+        return (node, domain, resource)
 
-    def __set_node(self,s):
-        """Initialize `self.node`
+    @staticmethod
+    def __prepare_node(data):
+        """Prepare node part of the JID
 
         :Parameters:
-            - `s`: Node part of the JID
+            - `data`: Node part of the JID
         :Types:
-            - `s`: unicode
+            - `data`: unicode
 
         :raise JIDError: if the node name is too long.
         :raise pyxmpp.xmppstringprep.StringprepError: if the
             node name fails Nodeprep preparation."""
-        if s:
-            s = unicode(s)
-            s=nodeprep.prepare(s)
-            if len(s.encode("utf-8"))>1023:
-                raise JIDError,"Node name too long"
-        else:
-            s=None
-        object.__setattr__(self,"node",s)
+        if not data:
+            return None
 
-    def __set_domain(self,s):
-        """Initialize `self.domain`
+        data = unicode(data)
+        node = nodeprep.prepare(data)
+        if len(node.encode("utf-8")) > 1023:
+            raise JIDError,"Node name too long"
+        return node
+
+    @staticmethod
+    def __prepare_domain(data):
+        """Prepare node part of the JID.
 
         :Parameters:
-            - `s`: Unicode or UTF-8 domain part of the JID
+            - `data`: Domain part of the JID
+        :Types:
+            - `data`: unicode
 
         :raise JIDError: if the domain name is too long."""
+        if not data:
+            raise JIDError, "Domain must be given"
+        data = unicode(data)
+        labels = data.split(u".")
+        labels = [idna.nameprep(label) for label in labels]
+        domain = ".".join(labels)
+        if len(domain.encode("utf-8")) > 1023:
+            raise JIDError, "Domain name too long"
+        return domain
 
-        if s is None:
-            raise JIDError,"Domain must be given"
-        if s:
-            s = unicode(s)
-        s=idna.nameprep(s)
-        if len(s.encode("utf-8"))>1023:
-            raise JIDError,"Domain name too long"
-        object.__setattr__(self,"domain",s)
-
-    def __set_resource(self,s):
-        """Initialize `self.resource`
+    @staticmethod
+    def __prepare_resource(data):
+        """Prepare resource part of the JID.
 
         :Parameters:
-            - `s`: Unicode or UTF-8 resource part of the JID
+            - `data`: Resource part of the JID
 
         :raise JIDError: if the resource name is too long.
         :raise pyxmpp.xmppstringprep.StringprepError: if the
             node name fails Resourceprep preparation."""
-        if s:
-            s = unicode(s)
-            s=resourceprep.prepare(s)
-            if len(s.encode("utf-8"))>1023:
-                raise JIDError,"Resource name too long"
-        else:
-            s=None
-        object.__setattr__(self,"resource",s)
+        if not data:
+            return None
+        data = unicode(data)
+        resource = resourceprep.prepare(data)
+        if len(resource.encode("utf-8")) > 1023:
+            raise JIDError, "Resource name too long"
+        return resource
 
     def __str__(self):
-        warnings.warn("JIDs should not be used as strings", DeprecationWarning, stacklevel=2)
+        warnings.warn("JIDs should not be used as strings",
+                                    DeprecationWarning, stacklevel = 2)
         return self.as_utf8()
 
     def __unicode__(self):
         return self.as_unicode()
 
     def __repr__(self):
-        return "<JID: %r>" % (self.as_unicode())
+        return "JID(%r)" % (self.as_unicode())
 
     def as_utf8(self):
         """UTF-8 encoded JID representation.
@@ -226,44 +228,44 @@ class JID(object):
         """Unicode string JID representation.
 
         :return: JID as Unicode string."""
-        r=self.domain
+        result = self.domain
         if self.node:
-            r=self.node+u'@'+r
+            result = self.node + u'@' + result
         if self.resource:
-            r=r+u'/'+self.resource
-        if not JID.cache.has_key(r):
-            JID.cache[r]=self
-        return r
+            result = result + u'/' + self.resource
+        if not JID.cache.has_key(result):
+            JID.cache[result] = self
+        return result
 
     def bare(self):
         """Make bare JID made by removing resource from current `self`.
 
         :return: new JID object without resource part."""
-        return JID(self.node,self.domain,check=False)
+        return JID(self.node, self.domain, check = False)
 
-    def __eq__(self,other):
+    def __eq__(self, other):
         if other is None:
             return False
         elif type(other) in (str, unicode):
             try:
-                other=JID(other)
+                other = JID(other)
             except:
                 return False
         elif not isinstance(other,JID):
             return False
 
-        return (self.node==other.node
-            and are_domains_equal(self.domain,other.domain)
-            and self.resource==other.resource)
+        return (self.node == other.node
+            and are_domains_equal(self.domain, other.domain)
+            and self.resource == other.resource)
 
     def __ne__(self,other):
         return not self.__eq__(other)
 
-    def __cmp__(self,other):
-        a=self.as_unicode()
-        return cmp(a,other)
+    def __cmp__(self, other):
+        uni = self.as_unicode()
+        return cmp(uni, other)
 
     def __hash__(self):
-        return hash(self.node)^hash(self.domain)^hash(self.resource)
+        return hash(self.node) ^ hash(self.domain) ^ hash(self.resource)
 
 # vi: sts=4 et sw=4
