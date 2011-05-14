391a866a4327eb9d175d7415129068de02fce327
Jacek Konieczny <jajcus@jajcus.net>
2011-05-14 21:06:44 +0200

    Initial implementation of the new Stanza
    stanza.py (Stanza class) totally rewritten, new .stanzapayload module
    added, providing the new StanzaPayload and XMLPayload classes.
    
    None of this has been tested in any way. Or finished.

---
+++ pyxmpp/stanzapayload.py
@@ -0,0 +1,58 @@
+#
+# (C) Copyright 2011 Jacek Konieczny <jajcus@jajcus.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License Version
+# 2.1 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+"""XMPP payload classes."""
+
+from abc import ABCMeta
+from copy import deepcopy
+from xml.etree import ElementTree
+
+class StanzaPayload:
+    """Abstract base class for stanza payload objects."""
+    __metaclass__ = ABCMeta
+
+    xml_namespace = None
+
+    def __init__(self, data):
+        raise NotImplementedError
+
+    def as_xml(self):
+        raise NotImplementedError
+
+    def copy(self):
+        return deepcopy(self)
+
+class XMLPayload(StanzaPayload):
+    """Transparent XML payload for stanza.
+    
+    This object can be used for any stanza payload.
+    It doesn't decode the XML element, but instead keeps it in the ElementTree
+    format."""
+    def __init__(self, data):
+        if isinstance(data, StanzaPayload):
+            data = data.as_xml()
+        if not isinstance(data, ElementTree.Element):
+            raise TypeError, "ElementTree.Element required"
+        if data.tag.startswith("{"):
+            self.xml_namespace = data.tag[1:].split("}", 1)[0]
+        else:
+            self.xml_namespace = None
+        self.element = data
+
+    def as_xml(self):
+        return self.element
+    
