1792a49841d4aedecd9614f6bd7468651fa76b2e
Jacek Konieczny <jajcus@jajcus.net>
2011-05-16 22:09:03 +0200

    More constants added
    More constants added, including the XML namespace, the XML lang
    attribute qname and *_QNP constants with qname prefixes coresponding
    to the *_NS constants

--- pyxmpp/constants.py
+++ pyxmpp/constants.py
@@ -17,9 +17,22 @@
 
 "Common XMPP constants."""
 
+XML_NS = "http://www.w3.org/XML/1998/namespace"
+
 STREAM_NS = "http://etherx.jabber.org/streams"
 
 STANZA_CLIENT_NS = "jabber:client"
 STANZA_SERVER_NS = "jabber:server"
 
 STANZA_NAMESPACES = (STANZA_CLIENT_NS, STANZA_SERVER_NS)
+
+STANZA_ERROR_NS='urn:ietf:params:xml:ns:xmpp-stanzas'
+STREAM_ERROR_NS='urn:ietf:params:xml:ns:xmpp-streams'
+PYXMPP_ERROR_NS='http://pyxmpp.jajcus.net/xmlns/errors'
+
+# build the _QNP (QName prefix) constants
+for name, value in globals().items():
+    if name.endswith("_NS"):
+        globals()[name[:-3] + "_QNP"] = "{{{0}}}".format(value)
+
+XML_LANG_QNAME = XML_QNP + "lang"
