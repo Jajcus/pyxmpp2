8c28d9ec1646760b35359701b2bda6b456814f27
Jacek Konieczny <jajcus@jajcus.net>
2011-05-20 21:25:19 +0200

    xml:lang support for XMPPSerializer.emit_head()

--- pyxmpp/xmppserializer.py
+++ pyxmpp/xmppserializer.py
@@ -86,17 +86,20 @@ class XMPPSerializer(object):
         """
         self._prefixes[namespace] = prefix
 
-    def emit_head(self, stream_from, stream_to, version = u'1.0'):
+    def emit_head(self, stream_from, stream_to, stream_id, version = u'1.0',
+                                                            language = None):
         """Return the opening tag of the stream root element.
 
         :Parameters:
             - `stream_from`: the 'from' attribute of the stream. May be `None`.
             - `stream_to`: the 'to' attribute of the stream. May be `None`.
             - `version`: the 'version' of the stream.
+            - `language`: the 'xml:lang' of the stream
         :Types:
             - `stream_from`: `unicode`
             - `stream_to`: `unicode`
             - `version`: `unicode`
+            - `language`: `unicode`
         """
         self._root_prefixes = dict(STANDARD_PREFIXES)
         self._root_prefixes[self.stanza_namespace] = None
@@ -112,6 +115,10 @@ class XMPPSerializer(object):
             tag += u" from={0}".format(quoteattr(stream_from))
         if stream_to:
             tag += u" to={0}".format(quoteattr(stream_to))
+        if stream_id is not None:
+            tag += u" id={0}".format(quoteattr(stream_id))
+        if language is not None:
+            tag += u" xml:lang={0}".format(quoteattr(stream_id))
         for namespace, prefix in self._root_prefixes.items():
             if prefix:
                 tag += u' xmlns:{0}={1}'.format(prefix, quoteattr(namespace))
