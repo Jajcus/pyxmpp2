5de63a9d04e4b291e4d72d989124ffd3e4816759
Jacek Konieczny <jajcus@jajcus.net>
2011-06-18 11:51:54 +0200

    Moving the main loop stuff to a subpackage

---
+++ pyxmpp/mainloop/__init__.py
@@ -0,0 +1,25 @@
+#
+# (C) Copyright 2011 Jacek Konieczny <jajcus@jajcus.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License Version
+# 2.1 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# pylint: disable-msg=W0201
+
+"""PyXMPP main event loop – the event and I/O framework, aka re-inventing the
+wheel."""
+
+from __future__ import absolute_import
+
+__docformat__ = "restructuredtext en"
+
