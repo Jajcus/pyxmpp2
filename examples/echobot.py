48fb8f19be3fc5b96c2f093cb0d2f75d14ea25f4
Jacek Konieczny <jajcus@jajcus.net>
2011-06-27 20:28:27 +0200

    Echo Bot, the PyXMPP2 edition

---
+++ examples/echobot.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+An 'echo bot' – simple client that just confirms any presence subscriptions
+and echoes incoming messages.
+"""
+
+import sys
+import logging
+from getpass import getpass
+import argparse
+
+from pyxmpp2.jid import JID
+from pyxmpp2.message import Message
+from pyxmpp2.presence import Presence
+from pyxmpp2.client import Client
+from pyxmpp2.settings import XMPPSettings
+from pyxmpp2.mainloop.interfaces import EventHandler, event_handler, QUIT
+from pyxmpp2.streamevents import AuthorizedEvent, DisconnectedEvent
+from pyxmpp2.interfaces import XMPPFeatureHandler
+from pyxmpp2.interfaces import presence_stanza_handler, message_stanza_handler
+
+class EchoBot(EventHandler, XMPPFeatureHandler):
+    """Echo Bot implementation."""
+    def __init__(self, my_jid, settings):
+        self.client = Client(my_jid, [self], settings)
+
+    def run(self):
+        """Request client connection and start the main loop."""
+        self.client.connect()
+        self.client.run()
+
+    def disconnect(self):
+        """Request disconnection and let the main loop run for a 2 more
+        seconds for graceful disconnection."""
+        self.client.disconnect()
+        self.client.run(timeout = 2)
+
+    @presence_stanza_handler("subscribe")
+    def handle_presence_subscribe(self, stanza):
+        logging.info(u"{0} requested presence subscription"
+                                                    .format(stanza.from_jid))
+        presence = Presence(to_jid = stanza.from_jid.bare(),
+                                                    stanza_type = "subscribe")
+        return [stanza.make_accept_response(), presence]
+
+    @presence_stanza_handler("subscribed")
+    def handle_presence_subscribed(self, stanza):
+        logging.info(u"{0!r} accepted our subscription request"
+                                                    .format(stanza.from_jid))
+        return True
+
+    @presence_stanza_handler("unsubscribe")
+    def handle_presence_unsubscribe(self, stanza):
+        logging.info(u"{0} canceled presence subscription"
+                                                    .format(stanza.from_jid))
+        presence = Presence(to_jid = stanza.from_jid.bare(),
+                                                    stanza_type = "unsubscribe")
+        return [stanza.make_accept_response(), presence]
+
+    @presence_stanza_handler("unsubscribed")
+    def handle_presence_unsubscribed(self, stanza):
+        logging.info(u"{0!r} acknowledged our subscrption cancelation"
+                                                    .format(stanza.from_jid))
+        return True
+
+    @message_stanza_handler()
+    def handle_message(self, stanza):
+        """Echo every non-error ``<message/>`` stanza.
+        
+        Add "Re: " to subject, if any.
+        """
+        if stanza.subject:
+            subject = u"Re: " + stanza.subject
+        else:
+            subject = None
+        msg = Message(stanza_type = stanza.stanza_type,
+                        from_jid = stanza.to_jid, to_jid = stanza.from_jid,
+                        subject = subject, body = stanza.body,
+                        thread = stanza.thread)
+        return msg
+
+    @event_handler(AuthorizedEvent)
+    def handle_authorized(self, event):
+        """Send the initial presence after log-in."""
+        event.stream.send(Presence())
+
+    @event_handler(DisconnectedEvent)
+    def handle_disconnected(self, event):
+        """Quit the main loop upon disconnection."""
+        return QUIT
+    
+    @event_handler()
+    def handle_all(self, event):
+        """Log all events."""
+        logging.info(u"-- {0}".format(event))
+
+def main():
+    """Parse the command-line arguments and run the bot."""
+    parser = argparse.ArgumentParser(description = 'XMPP echo bot')
+    parser.add_argument('jid', metavar = 'JID', 
+                                        help = 'The bot JID')
+    parser.add_argument('password', metavar = 'PASSWORD', nargs = '?',
+                                        help = 'Password for the JID account')
+    parser.add_argument('--tls', 
+                            action = 'store_true', dest = 'tls_enable', 
+                            default = False, help = 'Enable StartTLS')
+    parser.add_argument('--no-tls-peer-verify', 
+                            action = 'store_false', dest = 'tls_verify_peer', 
+                            default = True, help = 'Enable StartTLS')
+    parser.add_argument('--server', metavar = 'SERVER',
+                        help = 'Server to connect to (default: SRV lookup)')
+    parser.add_argument('--debug',
+                        action = 'store_const', dest = 'log_level',
+                        const = logging.DEBUG, default = logging.INFO,
+                        help = 'Print debug messages')
+    parser.add_argument('--quiet', const = logging.ERROR,
+                        action = 'store_const', dest = 'log_level',
+                        help = 'Print only error messages')
+
+    args = parser.parse_args()
+    if not args.password:
+        args.password = getpass("{0!r} password: ".format(args.jid))
+
+    if sys.version_info.major < 3:
+        args.jid = args.jid.decode("utf-8")
+        args.password = args.password.decode("utf-8")
+        if args.server is not None:
+            args.server = args.server.decode("utf-8")
+
+    logging.basicConfig(level = args.log_level)
+    settings = XMPPSettings({
+                                "password": args.password,
+                                "tls_enable": args.tls_enable,
+                                "tls_verify_peer": args.tls_verify_peer,
+                                })
+    if args.server:
+        settings["server"] = args.server
+
+    bot = EchoBot(JID(args.jid), settings)
+    try:
+        bot.run()
+    except KeyboardInterrupt:
+        bot.disconnect()
+
+if __name__ == '__main__':
+    main()
