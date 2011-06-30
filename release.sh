ab5743befde5e57c94d04bc99045affdb837fe3d
Jacek Konieczny <jajcus@jajcus.net>
2011-06-30 13:17:21 +0200

    PyPI register with no '--dry-run' on release

--- release.sh
+++ release.sh
@@ -48,7 +48,7 @@ git checkout "$release" || exit 1
 if sed -i -e's/^version.*/version = "'$release'"/' setup.py ; then
 	rm -f pyxmpp/version.py 2>/dev/null
 	if make dist ; then
-		python setup.py --dry-run --verbose register --strict
+		python setup.py --verbose register --strict
 	fi
 fi
 git checkout setup.py
