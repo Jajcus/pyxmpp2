6b72bd8d2c30acfd5d5971358ded97d6f03f8eb5
Jacek Konieczny <jajcus@jajcus.net>
2011-07-08 20:15:51 +0200

    RosterItem.as_xml() fixes

--- pyxmpp2/roster.py
+++ pyxmpp2/roster.py
@@ -228,17 +228,19 @@ class RosterItem(object):
         :Types:
             - `parent`: :etree:`ElementTree.Element`
         """
-        if parent:
+        if parent is not None:
             element = ElementTree.SubElement(parent, ITEM_TAG)
         else:
             element = ElementTree.Element(ITEM_TAG)
-        element.set("jid", self.jid)
-        element.set("name", self.name)
-        element.set("subscription", self.subscription)
+        element.set("jid", unicode(self.jid))
+        if self.name is not None:
+            element.set("name", self.name)
+        if self.subscription is not None:
+            element.set("subscription", self.subscription)
         if self.ask:
             element.set("ask", self.ask)
         if self.approved:
-            element.set("approved", self.approved)
+            element.set("approved", "true")
         for group in self.groups:
             ElementTree.SubElement(element, GROUP_TAG).text = group
         return element
