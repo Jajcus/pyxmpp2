68808ab898d05303b6d3bfc8c7094bb9286486bc
Jacek Konieczny <jajcus@jajcus.net>
2011-07-09 19:34:17 +0200

    MainLoop: delayed_call method added
    MainLoop.delayed_call() method for one-shot delayed method calls

--- pyxmpp2/mainloop/interfaces.py
+++ pyxmpp2/mainloop/interfaces.py
@@ -260,6 +260,30 @@ class MainLoop:
             - `handler`: `IOHandler` or `EventHandler` or `TimeoutHandler`
         """
         pass
+    
+    def delayed_call(self, delay, function):
+        """Schedule function to be called from the main loop after `delay`
+        seconds.
+
+        :Parameters:
+            - `delay`: seconds to wait
+        :Types:
+            - `delay`: `float`
+        """
+        main_loop = self
+        handler = []
+        class DelayedCallHandler(TimeoutHandler):
+            """Wrapper timeout handler class for the delayed call."""
+            # pylint: disable=R0903
+            @timeout_handler(delay, False)
+            def callback(self):
+                """Wrapper timeout handler method for the delayed call."""
+                try:
+                    function()
+                finally:
+                    main_loop.remove_handler(handler[0])
+        handler.append(DelayedCallHandler())
+        self.add_handler(handler[0])
 
     @abstractmethod
     def quit(self):
