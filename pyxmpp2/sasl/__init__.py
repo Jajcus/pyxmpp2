8e201b0f3883efb8f7901d256480c489535c2c6a
Jacek Konieczny <jajcus@jajcus.net>
2011-07-14 13:28:51 +0200

    A function to filter SASL mechanism lists
    pyxmpp2.sasl.filter_mechanism_list()
    
    Will remove undesired mechanisms names from a list.

--- pyxmpp2/sasl/__init__.py
+++ pyxmpp2/sasl/__init__.py
@@ -80,4 +80,35 @@ def server_authenticator_factory(mechanism, password_manager):
     authenticator = SERVER_MECHANISMS_D[mechanism]
     return authenticator(password_manager)
 
-# vi: sts=4 et sw=4
+def filter_mechanism_list(mechanisms, properties, allow_insecure = False,
+                            server_side = False):
+    """Filter a mechanisms list only to include those mechanisms that cans
+    succeed with the provided properties and are secure enough.
+
+    :Parameters:
+        - `mechanisms`: list of the mechanisms names
+        - `properties`: available authentication properties
+        - `allow_insecure`: allow insecure mechanisms
+    :Types:
+        - `mechanisms`: sequence of `unicode`
+        - `properties`: mapping
+        - `allow_insecure`: `bool`
+
+    :returntype: `list` of `unicode`
+    """
+    result = []
+    for mechanism in mechanisms:
+        if server_side:
+            klass = SERVER_MECHANISMS_D[mechanism]
+        else:
+            klass = CLIENT_MECHANISMS_D[mechanism]
+        secure = properties.get("security-layer")
+        if not allow_insecure and not klass._pyxmpp_sasl_secure and not secure:
+            logger.debug(" skipping {0}, as it is not secure".format(mechanism))
+            continue
+        if not klass.are_properties_sufficient(properties):
+            logger.debug(" skipping {0}, as the properties are not sufficient"
+                                                            .format(mechanism))
+            continue
+        result.append(mechanism)
+    return result
