40809f2f77bbc53e9bad7bad69435c47227e597e
lilydjwg <lilydjwg@gmail.com>
2012-12-07 19:12:46 +0800

    hup when connecting, just try next address
    tornado doesn't distinguish hup and error; let's try to cope with it.

--- pyxmpp2/transport.py
+++ pyxmpp2/transport.py
@@ -762,6 +762,11 @@ class TCPTransport(XMPPTransport, IOHandler):
         Handle the 'channel hungup' state. The handler should not be writable
         after this.
         """
+        with self.lock:
+            if self._state == 'connecting' and self._dst_addrs:
+                self._hup = False
+                self._set_state("connect")
+                return
         self._hup = True
 
     def handle_err(self):
