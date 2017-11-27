1f1aab0881dbddfb59cafce137a49b8cbdbb4d9c
Jacek Konieczny <jajcus@jajcus.net>
2017-11-27 21:05:30 +0100

    Version: 2.0.0
    Not finished, or anything. Still 'alpha quality', but with regular
    version number now.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 from setuptools import setup
 
-version = "2.0alpha2-git"
+version = "2.0.0"
 
 if (not os.path.exists(os.path.join("pyxmpp2","version.py"))
                                     or "make_version" in sys.argv):
